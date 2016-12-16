76d5e2c793d04c2112052f87ac89154c37ba43f6
David Dias <daviddias.p@gmail.com>
2016-12-16 10:12:49 +0000

    feat: vd + tests

---
+++ test/varint-decoder.spec.js
@@ -0,0 +1,45 @@
+/* eslint-env mocha */
+'use strict'
+
+const expect = require('chai').expect
+
+const vd = require('../src')
+
+describe('varint-decoder', () => {
+  it('decode 1 varint', () => {
+    const buf = new Buffer('05', 'hex')
+    const arr = vd(buf)
+    expect(arr[0]).to.equal(5)
+  })
+
+  it('decode 2 varints', () => {
+    const buf = new Buffer('000a', 'hex')
+    const arr = vd(buf)
+    expect(arr[0]).to.equal(0)
+    expect(arr[1]).to.equal(10)
+  })
+
+  it('decode 3 varints', () => {
+    const buf = new Buffer('0b0c03', 'hex')
+    const arr = vd(buf)
+    expect(arr[0]).to.equal(11)
+    expect(arr[1]).to.equal(12)
+    expect(arr[2]).to.equal(3)
+  })
+
+  it('decode 1 long varint', () => {
+    const buf = new Buffer('c801', 'hex')
+    const arr = vd(buf)
+    expect(arr[0]).to.equal(200)
+  })
+
+  it('decode a mix of long and short', () => {
+    const buf = new Buffer('96130208b90a', 'hex')
+    const arr = vd(buf)
+    expect(arr[0]).to.equal(2454)
+    expect(arr[1]).to.equal(2)
+    expect(arr[2]).to.equal(8)
+    expect(arr[3]).to.equal(1337)
+  })
+})
+
